8fa0c71239efc147ad6497412679e5cfed3895c5
gavinyahna <gavinyahna2@gmail.com>
2022-07-28 19:49:52 -0600

    fix text and background formatting and add view button to home page

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -3,25 +3,26 @@ import Button from '@mui/material/Button';
 import {Link} from "react-router-dom";
 import ContentBox from "../components/contentBox/ContentBox";
 import YoutubePlayer from "../components/youtubePlayer/YoutubePlayer";
+import "./Games.css";
 
 const Games = () => {
     return (
-        <div className="container">
+        <div className="container games-box">
             <ContentBox className="left">
                 <h2>Icy Boi</h2>
-                <p className="intro-text">
+                <p className="games-text">
                     Icy Boi was the first game I completed, it is a 2d platformer with wall climbing
                     movement mechanics and a snowball projectile that can be used to shoot enemies
                 </p>
-                <p className="intro-text">
+                <p className="games-text">
                     Icy Boi can be downloaded from my Itch.io account if you want to try it out:
-                    <a href="https://sp4ceboy.itch.io/icy-boi" target="_blank" rel="noreferrer">
-					    <Button className="btn" variant= "contained">Download Game</Button>
-				    </a>
                 </p>
+                <a href="https://sp4ceboy.itch.io/icy-boi" target="_blank" rel="noreferrer">
+                        <Button className="btn" variant= "contained">Download Game</Button>
+                    </a>
                 
-                <h3>Engine and Frameworks</h3>
-                <p className="intro-text">
+                <h3 className="games-text">Engine and Frameworks</h3>
+                <p className="games-text">
                     Unity Engine <br/>
                     Coded in C# .net Frameworks <br/>
                     Visuals created in Adobe Photoshop
@@ -30,20 +31,28 @@ const Games = () => {
 			</ContentBox>
             <ContentBox className="right">
                 <h2>Particle Shooter</h2>
-                <p className="intro-text">
+                <p className="games-text">
                     Particle shooter is still a work in progress, the main gameplay mechanic
                     is a terraforming rifle which shoots particles of different elements that
-                    effect the environment in the following ways: <br/>
-                    Ice: Allows player to move faster on a surface however they are not able to
-                    climb slopes with a 15% grade or steeper <br/>
-                    Grass: Player is able to climb surfaces up to a 45% grade however they are
-                    no longer able to run and have an additional movement speed penalty <br/>
-                    Fire: Resets both Ice and Grass blocks back to rock which has normal movement
-                    speed and a max climbing grade of 30%<br/>
-                    Energy: Deals damage to enemies<br/>
+                    effect the environment in the following ways:
                 </p>
-                <h3>Engine and Frameworks</h3>
-                <p className="intro-text">
+                <p className="blue text">
+                    Ice: Allows player to move faster on a surface however they are not able to
+                    climb slopes with a 15% grade or steeper
+                </p>
+                <p className="green text">
+                    Grass: Player is able to climb surfaces up to a 45% grade however they are
+                    no longer able to run and have an additional movement speed penalty
+                </p>
+                <p className="red text">
+                    Fire: Resets both Ice and Grass blocks back to rock which has normal movement
+                    speed and a max climbing grade of 30%
+                </p>
+                <p className="yellow text">
+                    Energy: Deals damage to enemies
+                </p>
+                <h3 className="games-text">Engine and Frameworks</h3>
+                <p className="games-text">
                     Unreal Engine 5 <br/>
                     Coded in C++
                 </p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,49 @@
 import React from "react";
+import Button from '@mui/material/Button';
 import PictureTabs from "../components/pictureTabs/PictureTabs"
 import ContentBox from "../components/contentBox/ContentBox"
 import portrait from "../../public/img/avatar.png"
 import "./Home.css";
 
-const Home = () => {
-  
-  return (
-    <div className="container">
-      <ContentBox className="left">
-        <img src={portrait} className="portrait"/>
-          <p className="intro-text">I am a fullstack software engineer with 3 years of professional experience working for UKG (Ultimate Kronos Group)</p>
-          <p className="intro-text">I have been developing software for 8 years mostly working on websites, webapps, and video games.</p>
-      </ContentBox>
-      <ContentBox className="right">
-        <p className="intro-text">Here are a few things I am passionate about</p>
-        <PictureTabs/>
-      </ContentBox>
-    </div>
-  );
-};
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {hide: false};
+  }
+
+  render() {
+    if (!this.state.hide) {
+    return (
+      <div className="container">
+        <ContentBox className="left">
+          <img src={portrait} className="portrait"/>
+            <p className="intro-text">I am a fullstack software engineer with 3 years of professional experience</p>
+            <p className="intro-text">I have been developing software for 7 years working on webapps and video games</p>
+            <p className="intro-text">
+              Check out my projects from the links in the navbar or 
+              <Button className="home-btn" variant= "contained" onClick={() =>this.setState({hide: true})}>
+                Enjoy the view
+              </Button>
+            </p>
+        </ContentBox>
+        <ContentBox className="right">
+          <p className="intro-text">Here are a few things I am passionate about</p>
+          <PictureTabs/>
+        </ContentBox>
+      </div>
+    );
+    } else {
+      return (
+        <div className="container">
+          <Button className="white-home-btn" variant= "contained" onClick={() =>this.setState({hide: false})}>
+            About Me
+          </Button>
+        </div>
+        
+      );
+    }
+  }
+}
 
 export default Home;
